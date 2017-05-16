cbd6b6d2185ba457b19c6484439c412dbd6a4fec
lelouedec <justin.lelouedec@gmail.com>
2017-05-16 11:15:44 +0200

    adding utils

---
+++ Utils.py
@@ -0,0 +1,20 @@
+from os import listdir
+from random import choice
+
+ext2conttype = {"jpg": "image/jpeg",
+                "jpeg": "image/jpeg",
+                "png": "image/png",
+                "gif": "image/gif"}
+
+def content_type(filename):
+    return ext2conttype[filename[filename.rfind(".")+1:].lower()]
+
+def isimage(filename):
+    """true if the filename's extension is in the content-type lookup"""
+    filename = filename.lower()
+    return filename[filename.rfind(".")+1:] in ext2conttype
+
+def random_file(dir):
+    """returns the filename of a randomly chosen image in dir"""
+    images = [f for f in listdir(dir) if isimage(f)]
+    return choice(images)
